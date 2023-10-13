e2d28d5e79173972bef1eea69f99583b0e167fd3
Deepak Singh <146908519+deepak-64742@users.noreply.github.com>
2023-10-13 11:24:32 +0530

    Delete _next/static directory

--- _next/static/ThxfDW7WUhz99dZBsncTy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/953-88f6448f6a03bfd2.js","static/chunks/pages/index-c825a79c5e2f6ba0.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
